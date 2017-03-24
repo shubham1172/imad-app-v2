ea6db26a548846de4601fa3ba84596831a8fef60
Shubham Sharma <shubhamsharma1172@gmail.com>
2017-03-24 22:16:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.status == 200) {
+       document.getElementById("counter").innerHTML = xhttp.responseText;
+    }
+};
+xhttp.open("GET", "/counter", true);
+xhttp.send();
