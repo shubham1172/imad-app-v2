4c2b11726726709af9501e77cef1c261403d1dae
Shubham Sharma <shubhamsharma1172@gmail.com>
2017-03-24 22:33:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/main.js', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'main.js'));    
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));    
 });
 
 counter = 0;
